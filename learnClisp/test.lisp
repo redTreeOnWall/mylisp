287efa820af5ec4a1d73fabf706cd211ba54eec0
lirunlong_dell <1249776253@qq.com>
2018-05-09 00:26:28 +0800

    lisper

---
+++ learnClisp/test.lisp
@@ -0,0 +1,33 @@
+(write-line "hello world")
+(write-line "hello world")  
+(write
+    '(+ 1 1)
+)
+
+(defun log (x)
+    (write x)
+    (write-line "")
+)
+
+(log 2)
+(log (+ 2 3))
+
+(defun loopl (n)
+        (log n)
+        (setq n (+ n 1))
+        (loopl n)
+)
+;又是一个似曾相识的感觉 flash back
+;; (loopl 0)
+
+(setq l '(+ 1 2))
+
+(log l)
+
+(defun getLam (x)
+    (lambda 
+    (n) (n+1) 
+    )
+)
+
+(log ((getLam 2) 2))
