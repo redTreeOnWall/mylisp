6de2ca2c36524a448fa8449b6a03f0385b7f0452
lee_centos <1249776253@qq.com>
2020-09-24 09:34:52 +0800

    c++

--- learnClisp/test.lisp
+++ learnClisp/test.lisp
@@ -1,22 +1,22 @@
 (write-line "hello world")
 (write-line "hello world")  
 (write
-    '(+ 1 1)
-)
+  '(+ 1 1)
+  )
 
 (defun log (x)
-    (write x)
-    (write-line "")
-)
+  (write x)
+  (write-line "")
+  )
 
 (log 2)
 (log (+ 2 3))
 
 (defun loopl (n)
-        (log n)
-        (setq n (+ n 1))
-        (loopl n)
-)
+  (log n)
+  (setq n (+ n 1))
+  (loopl n)
+  )
 ;又是一个似曾相识的感觉 flash back
 ;; (loopl 0)
 
@@ -25,9 +25,9 @@
 (log l)
 
 (defun getLam (x)
-    (lambda 
+  (lambda 
     (n) (n+1) 
     )
-)
+  )
 
 (log ((getLam 2) 2))
